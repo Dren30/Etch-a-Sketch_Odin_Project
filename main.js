f20360e0c9f9ac02a79de6588e4a602c1c348a27
Schavitt <schavittu.a30@gmail.com>
2024-07-21 00:52:54 -0400

    add button to change grid squares amount

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ header.textContent = "Etch-A-Sketch"
 
 let body = document.querySelector("body");
 body.appendChild(header);
+
+let changeGridButton = document.createElement("button");
+changeGridButton.textContent = "Change Grid";
+changeGridButton.classList.add("btn");
+changeGridButton.classList.add("grid-btn");
+body.appendChild(changeGridButton);
