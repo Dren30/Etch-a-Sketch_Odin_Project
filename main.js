3c49d30ac6dc72545494a16208d83c427e7f51eb
Schavitt <schavittu.a30@gmail.com>
2024-07-21 00:57:57 -0400

    Add sketch-Area to paint

--- main.js
+++ main.js
@@ -9,3 +9,13 @@ changeGridButton.textContent = "Change Grid";
 changeGridButton.classList.add("btn");
 changeGridButton.classList.add("grid-btn");
 body.appendChild(changeGridButton);
+
+const GRIDSIDE = 900;
+let squaresPerSide = 16;
+
+let sketchArea = document.createElement("div");
+sketchArea.classList.add("sketch-Area");
+sketchArea.style.width = `${GRIDSIDE}px`;
+sketchArea.style.height = `${GRIDSIDE}px`;
+
+body.appendChild(sketchArea);
