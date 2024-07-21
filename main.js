cc585ee4003f25562321c8a71fa207ef30af39b8
Schavitt <schavittu.a30@gmail.com>
2024-07-21 01:11:02 -0400

    Add color-buttons to select from

--- main.js
+++ main.js
@@ -23,3 +23,24 @@ body.appendChild(sketchArea);
 let buttonsArea = document.createElement("div");
 buttonsArea.classList.add("btns-Area");
 
+let paintBlackButton = document.createElement("button");
+paintBlackButton.textContent = "Paint Black"
+paintBlackButton.classList.add("btn");
+
+
+let paintRedButton = document.createElement("button");
+paintRedButton.textContent = "Paint Red"
+paintRedButton.classList.add("btn");
+
+
+let randomColorButton = document.createElement("button");
+randomColorButton.textContent = "Random Colors"
+randomColorButton.classList.add("btn");
+
+
+buttonsArea.appendChild(paintBlackButton);
+buttonsArea.appendChild(paintRedButton);
+buttonsArea.appendChild(randomColorButton);
+
+
+body.appendChild(buttonsArea);
